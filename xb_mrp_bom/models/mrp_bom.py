a25fe303dc1b5fc925dc4cea8653d44d1ac01a12
Vo Huy Hoang <huyhoangvo2k1@gmail.com>
2026-01-11 23:13:46 +0700

    fix bom productions

--- xb_mrp_bom/models/mrp_bom.py
+++ xb_mrp_bom/models/mrp_bom.py
@@ -130,9 +130,9 @@ class MrpBom(models.Model):
             for bom in self:
                 productions = self.env['mrp.production'].sudo().search([
                     '|',
-                    ('product_id', '=', bom.product_id.id),
-                    ('product_id.product_tmpl_id', '=', bom.product_tmpl_id.id),
-                    ('vehicle_station_id', '=', bom.workcenter_id.id),
+                    ('bom_id.product_id', '=', bom.product_id.id),
+                    ('bom_id.product_tmpl_id', '=', bom.product_tmpl_id.id),
+                    ('bom_id.workcenter_id', '=', bom.workcenter_id.id),
                     ('state_concrete', 'in', ['draft', 'assigned'])
                 ])
                 if productions and (last_version_bom := bom._get_last_version_bom(active_test=True)):
