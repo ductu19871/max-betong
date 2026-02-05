b8a8d249f184e2c5b6cdefff0bf7fb6808999fd6
Vo Huy Hoang <huyhoangvo2k1@gmail.com>
2026-02-05 17:48:46 +0700

    [IMP-9525] Adjust views betong

---
+++ xb_mrp_betong/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "MRP Betong custom by Xboss",
+    "summary": "MRP Betong custom by Xboss",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Xboss",
+    "depends": [
+        "max_betong_sale",
+        "xb_job_costing_management",
+        'sh_register_payment_so_po'
+    ],
+    "data": [
+        'views/sale_order_views.xml',
+    ],
+    "installable": True,
+}
