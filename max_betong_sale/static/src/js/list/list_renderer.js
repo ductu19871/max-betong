477f4e5b6d8148c43cd67735d6bdac5c9d61fd92
hoadx <hoadx@arrowhitech.com>
2026-02-09 22:09:13 +0700

    [MEDIUM][TASK - 9132] [Điều Phối Bê Tông] On-Time Delivery Report

--- max_betong_sale/static/src/js/list/list_renderer.js
+++ max_betong_sale/static/src/js/list/list_renderer.js
@@ -12,13 +12,10 @@ export class OnTimeListRenderer extends ListRenderer {
     }
 
     get aggregates() {
-        debugger;
         var aggregates = super.aggregates;
         var list = this.props.list;
-
         var is_late_data = list.records.filter((record) => record.data.is_late);
         var is_on_time_data = list.records.filter((record) => record.data.is_on_time);
-
         aggregates.is_late = {
             help: _t("Is Late (%)"),
             value: list.records.length > 0 ? `${is_late_data.length / list.records.length * 100}%` : '0%',
