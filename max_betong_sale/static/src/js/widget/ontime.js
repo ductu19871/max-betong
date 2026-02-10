dc9be40099ac81d4ea1fbc6cbfb151f9a20b8c6e
hoadx <hoadx@arrowhitech.com>
2026-02-10 08:55:35 +0700

    [MEDIUM][TASK - 9132] [Điều Phối Bê Tông] On-Time Delivery Report

---
+++ max_betong_sale/static/src/js/widget/ontime.js
@@ -0,0 +1,43 @@
+/** @odoo-module **/
+
+import { Component } from '@odoo/owl';
+import { BooleanField } from "@web/views/fields/boolean/boolean_field";
+import { registry } from '@web/core/registry';
+
+
+export class OntimeComponent extends Component {
+    static template = 'max_betong_sale.ontime';
+
+    get getTitle() {
+        let fieldName = this.props.name;
+        let record = this.props.record;
+
+        if (fieldName === 'is_late' && !record.data.is_late) {
+            return 'Is Late';
+        }
+        if (fieldName === 'is_on_time' && !record.data.is_on_time) {
+            return 'Is On Time';
+        }
+        return '';
+    }
+
+    get getBadgeClass() {
+        let fieldName = this.props.name;
+        let record = this.props.record;
+
+        if (fieldName === 'is_late' && !record.data.is_late) {
+            return 'danger';
+        }
+        if (fieldName === 'is_on_time' && !record.data.is_on_time) {
+            return 'success';
+        }
+        return '';
+    }
+}
+
+export const ontimeWidget = {
+    ...BooleanField,
+    component: OntimeComponent,
+};
+
+registry.category('fields').add('ontime', ontimeWidget);
