fbc6bec545650cb2db6c552bed8779c07cfad53b
DucTu <ductu1987@gmail.com>
2026-03-12 13:48:40 +0700

    [FIX7][LOW][TASK - 9692] Tách view MO, Ẩn một vài chức năng, Chuẩn hóa trạng thái, Ràng buộc dữ liệu về khối lượng, Update dump

--- max_betong_sale/models/mrp_production.py
+++ max_betong_sale/models/mrp_production.py
@@ -404,7 +404,6 @@ class Production(models.Model):
             'default_company_id': self.company_id.id or self.env.company.id,
             'default_ticket_on_hold_id': self.id,
             'default_ticket_on_hold_type': 'remix',
-            'default_load_id': self.load_id.id,
             'default_product_id': self.product_id.id
         }
         action['views'] = [(self.env.ref('max_betong_sale.mrp_production_ticket_on_hold_wizard_view').id, 'form')]
