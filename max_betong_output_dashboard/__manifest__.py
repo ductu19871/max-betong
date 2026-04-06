c9bc077ab8670b7a99852f531d5ad967c59f4eca
DucTu <ductu1987@gmail.com>
2026-04-06 11:00:46 +0700

    s2

--- max_betong_output_dashboard/__manifest__.py
+++ max_betong_output_dashboard/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Sales/Sales',
     'summary': "Kế hoach sản lượng - thực tế",
     'description': "Dashboard Báo cáo sản lượng kế hoạch và thực tế",
-    'depends': ['base', 'web', 'sale', 'project'],
+    'depends': ['base', 'web', 'sale', 'project', 'deliverable_payment_plan'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
